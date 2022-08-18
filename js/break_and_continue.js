168edd09677bd38f9c03ce873464f496a8e8c877
John Pedrotti <jdpedrotti@gmail.com>
2022-08-18 14:08:49 -0500

    break and continue excersize

---
+++ js/break_and_continue.js
@@ -0,0 +1,20 @@
+function isNumber(value) {
+    return !(isNaN(value) || typeof value === "boolean");
+}
+
+while(true){
+    let oddNumber = prompt("Please enter an odd number: ");
+    if(oddNumber >= 1 && oddNumber <= 50 && oddNumber % 2 === 1 && isNumber(oddNumber)){
+        console.log("Number to skip is: " + oddNumber);
+        console.log(" ");
+        for(let i = 1; i < 51; i += 2){
+            if(i == oddNumber){
+                console.log("Yikes! Skipping number: " + oddNumber)
+                continue;
+            } else{
+                console.log("Here is an odd number: " + i);
+            }
+        }
+        break;
+    }
+}
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,28 +1,52 @@
 // //1
-// function showMultiplicationTable (number){
+
+// function showMultiplicationTable (){
+//     let number = prompt("Enter a number: ")
 // for(i =1; i <= 10; i++) {
 //     console.log(`${number} * ${i} = ${number * i}`);
 //     }
 // }
+// showMultiplicationTable();
+
 
 // //2
+
 // for(let i = 1; i <= 10 ; i ++ ){
-//     number = Math.floor(Math.random() * (180) + 20);
+//     number = Math.floor(Math.random() * (181) + 20);
 //     if (number % 2 !== 0) {
-//         console.log(`${number} is even`)
-//     }else{
 //         console.log(`${number} is odd`)
+//     }else{
+//         console.log(`${number} is even`)
 //     }
 // }
 
 //3
+
+// below is loop within loop example
 // for(let i = 1; i <= 9; i ++ ){
 //     for(let j = 1; j <= i ; j ++){
 //         console.log(i)
 //     }
 // }
 
+// can use built in repeat function to repeat string and achieve same output
+// for (let i = 1; i <= 9; i ++) {
+//     console.log(i.toString().repeat(i));
+// }
+
 //4
-// for(let i = 100; i >= 0; i -= 5){
+
+// for(let i = 100; i >= 5; i -= 5){
 //     console.log(i);
+// }
+
+// let string = "";
+// for(let i = 0; i < 8; i ++){
+//     let word = prompt("Let's make a sentence! Enter a word: ");
+//     if(word === "curses"){
+//         console.log("That word is not allowed!");
+//         continue;
+//     }
+//     string += " " + word;
+//     console.log("Your sentence is: " + string);
 // }
