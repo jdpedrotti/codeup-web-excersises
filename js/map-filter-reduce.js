4b48e74e4890b87fa4ea5ee900fd2e11f51d81af
John Pedrotti <jdpedrotti@gmail.com>
2022-09-29 15:34:00 -0500

    map filter reduce lecture

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,110 @@
+const prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+let output = '';
+
+
+// prices.forEach((price, index, array) => {
+//
+//     const tax = (price * 0.0825).toFixed(2);
+//     const total = (price + parseFloat(tax)).toFixed(2);
+//     output += `<p>Item number ${index + 1}. Price $${price}. Tax: $${tax}. Total: $${total}</p>`;
+//     console.log(output);
+//     $("div").html(output);
+//
+//     if (index === array.length-1){
+//         $("div").append("<p>Thats it!!</p>")
+//     }
+// });
+
+const pricesAfterTax = prices.map(price =>{
+        const tax = (price * 0.0825).toFixed(2);
+        const total = (price + parseFloat(tax)).toFixed(2);
+        return parseFloat(total);
+});
+console.log(prices)
+console.log(pricesAfterTax);
+
+const doubledArray = prices.map(price => price * 2);
+console.log(doubledArray);
+
+// const desserts = ['sherbert', 'whiskey cake', 'cupcake', 'eclair'];
+//
+// const eatingDessert = desserts.map(dessert => `Eating ${dessert}`);
+// eatingDessert.forEach(element => {
+//     $("div").append(`<p>${element}</p>`);
+// });
+
+const cars = [
+    {
+        make: "Honda",
+        model: "Civic",
+        mileage: 10428
+    },
+    {
+        make: "Toyota",
+        model: "Corolla",
+        mileage: 9425
+    },
+    {
+        make: "Ford",
+        model: "Mustang",
+        mileage: 2567
+    },
+    {
+        make: "Audi",
+        model: "A3",
+        mileage: 14500
+    },
+    {
+        make: "Mazda",
+        model: "3",
+        mileage: 11248
+    }
+];
+
+const mileages = cars.map(car => car.mileage);
+console.log(mileages);
+
+const newObject = cars.map(car => {
+    car.newProp = 'new property';
+    return car;
+});
+
+console.log(cars[0]);
+
+
+const under10K = cars.filter(car => car.mileage < 10000);
+
+under10K.forEach(({make, model, mileage}) => {
+    output += `<p>I found a ${make} ${model} with ${mileage} miles</p>`;
+    $("#output").html(output)
+});
+
+
+const under10DollarsWithTax = prices.filter(price => price < 10)
+    .map(price => {
+        const tax = (price * 0.0825).toFixed(2);
+        const total = (price + parseFloat(tax)).toFixed(2);
+        return parseFloat(total);
+    });
+    // .forEach(price => $("div").append(`<p>${price}</p>`));
+
+//  chained .filter and .map above
+// console.log(under10DollarsWithTax);
+console.log(under10DollarsWithTax);
+
+
+const totalCost = prices.reduce(function (total, price){
+   return total + price;
+});
+
+console.log(totalCost);
+
+// const prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+
+// at index 0 it is similar to doing let total = arrayElement[0]
+
+let total = prices.reduce(function(total, itemPrice, index){
+    console.log(`The index is ${index}, the total is ${total}, the itemPrice is ${itemPrice}`);
+    return total + itemPrice;
+});
+$("#output").append(`<p>The total is ${totalCost}</p>`);
