67e8b2ce2be85912a1fc59ff1dfa58b689ec87e4
John_Pedrotti <jdpedrotti@gmail.com>
2022-10-04 14:25:26 -0500

    added async example to promises js

--- js/promises.js
+++ js/promises.js
@@ -11,6 +11,18 @@ function getLastPush(username){
         });
 }
 
+
+//  below is an example with async instead of a bunch of .then
 const lastPushPromise = getLastPush('jdpedrotti');
 lastPushPromise.then(value => console.log(value)).catch(error => console.log(error))
 
+async function getUserLastCommitAsync(username) {
+    try {
+        let response = await fetch(`https://api.github.com/users/${username}`);
+        let events = await response.json();
+        console.log(events);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
